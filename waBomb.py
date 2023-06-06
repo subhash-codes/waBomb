cc7bf7acea3248d5f0bfb6453bb957f085d1a990
Subhash Ramteke <93515047+subhash-codes@users.noreply.github.com>
2023-06-06 22:57:54 +0530

    waBomb.py

--- waBomb.py
+++ waBomb.py
@@ -1,8 +1,9 @@
+#!/bin/python
 import pyautogui
 import time
 time.sleep(5)
 count=0
 while count <=100:
-    pyautogui.typewrite("Butke futke miss you....")
+    pyautogui.typewrite("You have been hacked!....")
     pyautogui.press("enter")
     count = count+1
